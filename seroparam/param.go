321803971bd96a889439d528219ff96386750741
bourneAc <bourne@gameternal.com>
2020-02-14 18:01:40 +0800

    add SIP7
    complete output v2
    
    remove a sip7
    
    update linux3 lib
    
    update linux4 lib
    
    update windows lib

--- seroparam/param.go
+++ seroparam/param.go
@@ -64,6 +64,14 @@ func SIP6() uint64 {
 	}
 }
 
+func SIP7() uint64 {
+	if is_dev {
+		return 0
+	} else {
+		return uint64(2680000)
+	}
+}
+
 const MAX_O_INS_LENGTH = int(2500)
 
 const MAX_O_OUT_LENGTH = int(10)
