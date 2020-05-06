c959ea666da86acffc355f3d83f104f1d208f73e
bourneAc <bourne@gameternal.com>
2020-05-06 14:24:27 +0800

    update
    update
    
    update
    
    update

--- seroparam/param.go
+++ seroparam/param.go
@@ -72,6 +72,14 @@ func SIP7() uint64 {
 	}
 }
 
+func SIP8() uint64 {
+	if is_dev {
+		return 50
+	} else {
+		return uint64(3205679)
+	}
+}
+
 const MAX_O_INS_LENGTH = int(2500)
 
 const MAX_O_OUT_LENGTH = int(10)
